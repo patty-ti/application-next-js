ea7a738dce46361b4e69a4021a0238e254504fd1
patty-ti <rs.paty@outlook.com>
2023-10-24 15:28:58 -0300

    Configurando Stripe

--- next/shop/next.config.js
+++ next/shop/next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+
+  images: {
+    domains: [
+      'files.stripe.com',
+    ],
+  },
+
+  experimental: {
+    images: {
+      allowFutureImage: true,
+    }
+  }
 }
 
 module.exports = nextConfig
